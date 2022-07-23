4d6e2e2b848c9ac85b8fb252dcc1f60dc12bf343
nikhil96widhani <nikhil96widhani@gmail.com>
2022-07-23 13:13:01 -0400

    First Commit

---
+++ General/705. Design HashSet/main.py
@@ -0,0 +1,63 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        self.hashset = list()
+
+    def findIndex(self, key):
+        """
+        :rtype: key if found else None
+        """
+        i = 0
+        for val in self.hashset:
+            if val == key:
+                return i
+            i += 1
+
+        return None
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        contains = self.findIndex(key)
+        if not contains:
+            self.hashset.append(key)
+        else:
+            return None
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        contains = self.findIndex(key)
+        if contains:
+            self.hashset.pop(contains)
+        else:
+            return None
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        contains = self.findIndex(key)
+        if contains is not None:
+            return True
+        else:
+            return False
+
+
+
+
+if __name__ == '__main__':
+    # Your MyHashSet object will be instantiated and called as such:
+    obj = MyHashSet()
+    obj.add(1)
+    obj.add(2)
+    a = obj.contains(1)
+    obj.add(2)
+    b = obj.contains(2)
+    obj.remove(2)
+    c = obj.contains(2)
