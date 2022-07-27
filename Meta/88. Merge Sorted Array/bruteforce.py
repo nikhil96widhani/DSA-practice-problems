9a9107b248c7f35ccd0e6ff57146e5e1f0be86eb
nikhil96widhani <nikhil96widhani@gmail.com>
2022-07-26 23:25:12 -0400

    additions

--- Meta/88. Merge Sorted Array/bruteforce.py
+++ Meta/88. Merge Sorted Array/bruteforce.py
@@ -23,8 +23,36 @@
 
 #         return nums1
 
-
+##### APPROACH 2 little bit faster
 # KEEPING RECORD OF LAST INDEX CHANGED TO MAKE LOOP FASTER
+# class Solution(object):
+#     def merge(self, nums1, m, nums2, n):
+#         """
+#         :type nums1: List[int]
+#         :type m: int
+#         :type nums2: List[int]
+#         :type n: int
+#         :rtype: None Do not return anything, modify nums1 in-place instead.
+#         """
+#         lastinsert_index = None
+#         while len(nums2[0:n]) > 0:
+#             for i, val in enumerate(nums1):
+#                 if lastinsert_index is None or i > lastinsert_index :
+#                     if val >= nums2[0]:
+#                         nums1.insert(i, nums2[0])
+#                         nums2.pop(0)
+#                         nums1.pop(-1)
+#                         lastinsert_index = i+1
+#                         break
+#                     elif i == m + n - len(nums2):
+#                         nums1.insert(i, nums2[0])
+#                         nums2.pop(0)
+#                         nums1.pop(-1)
+#                         break
+#
+#         return nums1
+
+# combining if and elseif
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -37,17 +65,11 @@ class Solution(object):
         lastinsert_index = 0
         while len(nums2[0:n]) > 0:
             for i in range(lastinsert_index, len(nums1)):
-                if nums1[i] >= nums2[0]:
+                if nums1[i] >= nums2[0] or i == m + n - len(nums2):
                     nums1.insert(i, nums2[0])
                     nums2.pop(0)
                     nums1.pop(-1)
                     lastinsert_index = i+1
-                    break
-                elif i == m + n - len(nums2):
-                    nums1.insert(i, nums2[0])
-                    nums2.pop(0)
-                    nums1.pop(-1)
-                    lastinsert_index = i + 1
                     break
 
         return nums1
@@ -55,5 +77,6 @@ class Solution(object):
 
 if __name__ == '__main__':
     sol = Solution()
-    ans = sol.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+    ans = sol.merge([2,0], 1, [1], 1)
+
     print(ans)
