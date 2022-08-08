211002a869dc0157dfcd77fce21492a7bf3b9095
nikhil96widhani <nikhil96widhani@gmail.com>
2022-08-07 23:13:12 -0400

    additions

--- General/Linked List/141. Linked List Cycle/main.py
+++ General/Linked List/141. Linked List Cycle/main.py
@@ -1,25 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
-    def hasCycle(self, head):
+    def detectCycle(self, head):
         """
         :type head: ListNode
-        :rtype: bool
-
+        :rtype: ListNode
         Notes: Using floyds tortoise and Hare algorithm
-        Assuming fast will loop back to a position which will be equal to slow sometime in the loop
+        Assuming fast will loop back to a position which will be equal to slow sometime in the loop,
+        when cycle is found means both are at same point, then check every next to find the match node
         """
         fast, slow = head, head
-
-        while fast and fast.next:
-            slow = slow.next
+        while fast and fast.next and fast.next.next:
             fast = fast.next.next
-
-            if slow == fast:
-                return True
-
-        return False
+            slow = slow.next
+            if fast == slow:
+                slow = head
+                while (slow is not fast):
+                    fast = fast.next
+                    slow = slow.next
+                return slow
+        return None
